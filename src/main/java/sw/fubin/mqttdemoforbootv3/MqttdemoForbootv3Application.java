85458083d1b54e777acc3853782f5afb5b8ba383
爱莹才会拼呢 <>
2025-04-19 09:02:42 +0800

    first commit

---
+++ src/main/java/sw/fubin/mqttdemoforbootv3/MqttdemoForbootv3Application.java
@@ -0,0 +1,13 @@
+package sw.fubin.mqttdemoforbootv3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqttdemoForbootv3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqttdemoForbootv3Application.class, args);
+    }
+
+}
---
+++ src/test/java/sw/fubin/mqttdemoforbootv3/MqttdemoForbootv3ApplicationTests.java
@@ -0,0 +1,56 @@
+package sw.fubin.mqttdemoforbootv3;
+
+import org.eclipse.paho.mqttv5.client.MqttConnectionOptions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.support.AbstractBeanDefinition;
+import org.springframework.beans.factory.support.BeanDefinitionBuilder;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.integration.mqtt.outbound.Mqttv5PahoMessageHandler;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.messaging.Message;
+
+@SpringBootTest
+class MqttdemoForbootv3ApplicationTests {
+
+
+    @Autowired
+    private DefaultListableBeanFactory beanFactory;
+    @Test
+    void test1() {
+        MqttConnectionOptions options = new MqttConnectionOptions();
+        options.setServerURIs(new String[]{"tcp://192.168.128.114:1883"});
+        options.setUserName("swzs");
+        options.setPassword("swzs".getBytes());
+        options.setCleanStart(false);
+        options.setAutomaticReconnect(true);
+
+        //关键问题
+        options.setSessionExpiryInterval(1800L);
+        String clientId = "test222";
+        BeanDefinitionBuilder builder = BeanDefinitionBuilder.genericBeanDefinition(Mqttv5PahoMessageHandler.class);
+        builder.addConstructorArgValue(options);
+        builder.addConstructorArgValue(clientId);
+        builder.addPropertyValue("async", false);
+        AbstractBeanDefinition beanDefinition = builder.getBeanDefinition();
+        beanFactory.registerBeanDefinition("output",beanDefinition );
+        Mqttv5PahoMessageHandler messageHandler = beanFactory.getBean("output", Mqttv5PahoMessageHandler.class);
+
+        for (int i = 1; i <= 1000; i++) {
+            String msg = "=================================test data indexxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx=====>" + i;
+            String topic = "testRate";
+            Message<String> mqttMessage = MessageBuilder.withPayload(msg).setHeader(MqttHeaders.TOPIC, topic)
+                    .setHeader(MqttHeaders.QOS, 2).build();
+            System.out.println("msg====>"+msg);
+            try {
+
+                messageHandler.handleMessage(mqttMessage);
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
+    }
+
+}
